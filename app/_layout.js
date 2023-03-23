c2c72a130e88ad619017c1843536bfbe4c00f80e
Vasilevski95 <djordje.vasilevski95@gmail.com>
2023-03-23 16:30:58 +0100

    first commit

---
+++ app/_layout.js
@@ -0,0 +1,34 @@
+import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
+
+const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  // I only want to show the homepage if the fonts have been loaded
+
+  if (!fontsLoaded) return null;
+
+  //If fonts are not loaded return null
+
+  return <Stack onLayout={onLayoutRootView} />;
+
+  //If the fonts are loaded return Stack
+};
+
+//With this I ensure that the fonts will be loaded
+
+export default Layout;
---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../assets/images/Djole.jpg";
+
+export default {
+  profile,
+};
